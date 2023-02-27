c1f0be02428e9131cc1b1f40b78f6f7b32c0d232
Liz Schley <liz.schley@advance-auto.com>
2023-02-27 13:42:32 -0500

    working docker compose and clean test

---
+++ src/main/java/qa/spring_cucumber/rts_qa_spring/RtsQaSpringApplication.java
@@ -0,0 +1,13 @@
+package qa.spring_cucumber.rts_qa_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtsQaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RtsQaSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/qa/spring_cucumber/rts_qa_spring/RtsQaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package qa.spring_cucumber.rts_qa_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtsQaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
