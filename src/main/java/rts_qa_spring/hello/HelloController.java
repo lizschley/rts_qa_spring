b1f8e0f7d7ca8867a1f5c7a844b440b72dca6750
Liz Schley <liz.schley@advance-auto.com>
2023-03-02 15:27:07 -0500

    Working Endpoint

--- src/main/java/rts_qa_spring/hello/HelloController.java
+++ src/main/java/rts_qa_spring/hello/HelloController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
     @RequestMapping("/")
     public String helloWorld(){
         return "Hello World!";
