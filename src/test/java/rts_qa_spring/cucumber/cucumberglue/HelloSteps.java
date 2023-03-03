e603bcb5080c4744dcfb5746b8f06e02f1cc38c8
Liz Schley <liz.schley@advance-auto.com>
2023-03-03 09:19:42 -0500

    Hello tests working

--- src/test/java/rts_qa_spring/cucumber/cucumberglue/HelloSteps.java
+++ src/test/java/rts_qa_spring/cucumber/cucumberglue/HelloSteps.java
@@ -4,21 +4,20 @@ import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.And;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.text.MessageFormat;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HelloSteps {
-
-    @LocalServerPort
-    String port;
-
-    ResponseEntity<String> lastResponse;
-
     @Autowired
     private TestRestTemplate testRestTemplate;
 
@@ -26,32 +25,16 @@ public class HelloSteps {
 
     @When("the client named {string} calls hello")
     public void callHello(String name) {
-        responseEntity = testRestTemplate.getForEntity("http://localhost:8083/examples/hello?name=" + name, String.class);
+        responseEntity = testRestTemplate.getForEntity("/hello?name=" + name, String.class);
     }
 
     @Then("the client receives status code of 200")
     public void checkStatus200(){
-
         assertEquals(responseEntity.getStatusCode() , HttpStatus.OK);
-    }
-
-    @Then("the client call sum with {int} and {int}")
-    public void callSum(int a, int b) {
-        responseEntity = testRestTemplate.getForEntity("http://localhost:8080/examples/sum?a=" + a +"&b=" + b,
-                String.class);
-
     }
 
     @And("the client with name {string} receives server hello message")
     public void checkHelloResponseValue(String name) {
-
         assertTrue(responseEntity.getBody().contains(name));
     }
-
-    @And("check results of sum correspond with expected result {int}")
-    public void checkResultOfSum(int expectedResult) {
-
-        assertEquals(Integer.parseInt(responseEntity.getBody()), expectedResult);
-    }
-
 }
--- src/test/java/rts_qa_spring/cucumber/cucumberglue/RunCucumberTest.java
+++ src/test/java/rts_qa_spring/cucumber/cucumberglue/RunCucumberTest.java
@@ -1,11 +1,10 @@
 package rts_qa_spring.cucumber.cucumberglue;
 
+import io.cucumber.junit.platform.engine.Constants;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
-
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-
 
 // see https://palashray.com/example-of-creating-cucumber-based-bdd-tests-using-junit5-and-spring-dependency-injection/
 // The Test Runner
@@ -13,5 +12,6 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 @Suite
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "rts_qa_spring.cucumber.cucumberglue")
+@ConfigurationParameter(key = Constants.PLUGIN_PUBLISH_QUIET_PROPERTY_NAME, value = "true")
 public class RunCucumberTest {
 }
