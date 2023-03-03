69da193f1c50134d8f98cd8035df7135f8f66833
Liz Schley <liz.schley@advance-auto.com>
2023-03-03 10:16:55 -0500

    Added cucumber output both in terminal and in json file

--- src/test/java/rts_qa_spring/cucumber/cucumberglue/RunCucumberTest.java
+++ src/test/java/rts_qa_spring/cucumber/cucumberglue/RunCucumberTest.java
@@ -5,6 +5,7 @@ import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 // see https://palashray.com/example-of-creating-cucumber-based-bdd-tests-using-junit5-and-spring-dependency-injection/
 // The Test Runner
@@ -13,5 +14,6 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "rts_qa_spring.cucumber.cucumberglue")
 @ConfigurationParameter(key = Constants.PLUGIN_PUBLISH_QUIET_PROPERTY_NAME, value = "true")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, json:target/cucumber-report/cucumber.json")
 public class RunCucumberTest {
 }
