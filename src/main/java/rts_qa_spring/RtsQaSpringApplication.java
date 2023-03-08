71d15fe8239298191d303969cbd1e3dc60685426
Liz Schley <liz.schley@advance-auto.com>
2023-03-08 15:59:04 -0500

    new strategy to try tomorrow

--- src/main/java/rts_qa_spring/RtsQaSpringApplication.java
+++ src/main/java/rts_qa_spring/RtsQaSpringApplication.java
@@ -1,29 +1,19 @@
 package rts_qa_spring;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import rts_qa_spring.quotes.LoadData;
 
 @SpringBootApplication
 public class RtsQaSpringApplication implements CommandLineRunner {
-
-    private static Logger logger = LoggerFactory
-            .getLogger(RtsQaSpringApplication.class);
-
     public static void main(String[] args) {
-        logger.info("1. STARTING : Spring boot application starting");
         SpringApplication.run(RtsQaSpringApplication.class, args);
-        logger.info("3. STOPPED  : Spring boot application end of main");
     }
 
     @Override
     public void run(String... args) throws Exception {
-        logger.info("2. EXECUTING : command line runner");
-
-        for(int i=0;i<=5;i++){
-            logger.info("Count = "+i);
-        }
+        LoadData classObj = new LoadData();
+        classObj.load_data();
     }
 }
